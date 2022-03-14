34a3a7c3c5252a3bb3993e010749076654fcb036
Anjali Maragoni <83865472+AnjaliMaragoni@users.noreply.github.com>
2022-03-14 16:06:04 +0530

    Update arrayCopyjava

--- assignment1/arrayCopy.java
+++ assignment1/arrayCopy.java
@@ -8,19 +8,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class arrayCopy {
-	public static void main(String[] args){
-		System.out.println("Enter Data in Array: ");
-		int array1[] = new int[5];
-		Scanner sc = new Scanner(System.in);
-		
-		for (int i=0;i<5;i++) {
-			array1[i] = sc.nextInt();
-		}
-		
-		int array2[] = Arrays.copyOf(array1,5);
-		System.out.print(" Copy Data in Array2: ");	
-		for (int i=0; i<5; i++) {
-			System.out.print(array2[i]+" ");
-		}		
+	public static void main(String[] args) 
+	{
+	    int n;
+	    Scanner sc=new Scanner(System.in);
+	    System.out.print("enter the size of array: ");
+	    n=sc.nextInt();
+	    int array1[]=new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        array1[i]=sc.nextInt();
+	    }
+	    System.out.println("elements in array1: ");
+	    for(int i=0;i<n;i++)
+	    {
+	        System.out.print(array1[i]+" ");
+	            
+	    }
+	    copyOf(array1);
+	}
+	public static int[] copyOf(int array1[])
+	{
+	    int n=array1.length;
+	    int array2[]=new int[n];
+	    array2=array1;
+	    System.out.println(" \nelements in array2 are: ");
+	    for(int i=0;i<n;i++)
+	    {
+	        System.out.print(array2[i]+" ");
+	            
+	    }
+	    return array2;
 	}
 }
